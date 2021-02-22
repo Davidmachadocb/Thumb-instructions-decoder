f1c7f9a4d4200ddcdf9958aa454df06011d2d3dd
Davidmachadocb <78983718+Davidmachadocb@users.noreply.github.com>
2021-02-22 00:47:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"map.h"
+#include"Decode.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "Decode.h"
+int menu(){
+    int i;
+	printf("----------MENU----------\n");
+	printf("|1 - EXECUTAR          |\n");
+	printf("|2 - EXIT              |\n");
+    printf("------------------------\n");
+	printf("Choose: "); scanf("%d",&i);
+	printf("\n");
+	return i;
+}
 
 int main(){
-
-    int number;
-
+    int escolha, number;
     FILE *ptr_file, *ptr_file2;
+    char line[10];
 
-    ptr_file = fopen("teste.txt", "r");
-    ptr_file2 = fopen("teste_copy.txt", "w");
+    do{
+        escolha = menu();
+        switch(escolha){
+            case(1):
 
-    if(ptr_file == NULL || ptr_file2 == NULL){
-        printf("Error!");
-        return 1;
-    }
+                ptr_file = fopen("input.txt", "r");
+                ptr_file2 = fopen("output.txt", "w");
 
-    while(fgets(line, 10, ptr_file) != NULL){
-        number = strtol (line,NULL,16);
-        Decode7(number, ptr_file2);
-    }
+                if(ptr_file == NULL || ptr_file2 == NULL){
+                    printf("Error!");
+                    return 1;
+                }
 
-    fclose(ptr_file);
-    fclose(ptr_file2);
+                while(fgets(line, 10, ptr_file) != NULL){
+                    number = strtol (line,NULL,16);
+                    mapFunction(number, ptr_file2);
+                }
 
+                fclose(ptr_file);
+                fclose(ptr_file2);
+                
+                printf("Programa executado com sucesso e finalizado!\n");
+                return 0;
 
+            break;
+
+            case(2):
+                printf("Acabou o programa\n");
+                return 0;
+            break;
+
+            default:
+                system("cls");
+                printf("escolha nao valida!!! escolha novamente\n");
+            break;
+        }
+    }while(escolha != 2);
     return 0;
 }
